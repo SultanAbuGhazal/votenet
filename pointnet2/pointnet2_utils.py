57835a4a76ba265dd9462cba8e2ef565fea090d3
Sultan Abu Ghazal <sultanmobeen@hotmail.com>
2021-07-14 20:11:12 +0400

    Update pointnet2_utils.py

--- pointnet2/pointnet2_utils.py
+++ pointnet2/pointnet2_utils.py
@@ -23,6 +23,8 @@ except:
     import __builtin__ as builtins
 
 try:
+    import pointnet2 as ppp
+    print(ppp)
     import pointnet2._ext as _ext
 except ImportError:
     if not getattr(builtins, "__POINTNET2_SETUP__", False):
